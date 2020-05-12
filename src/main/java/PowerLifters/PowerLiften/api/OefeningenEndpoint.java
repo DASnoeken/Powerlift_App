61af9912bd81c6099a2751973718c303815a321f
JoeriJanssen <JoeriJanssen@live.nl>
2020-05-12 14:28:16 +0200

    laatOefeningZien

--- src/main/java/PowerLifters/PowerLiften/api/OefeningenEndpoint.java
+++ src/main/java/PowerLifters/PowerLiften/api/OefeningenEndpoint.java
@@ -1,5 +1,23 @@
 package PowerLifters.PowerLiften.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import PowerLifters.PowerLiften.controller.OefeningenService;
+import PowerLifters.PowerLiften.domein.Oefening;
+
+@RestController
 public class OefeningenEndpoint {
+	@Autowired
+	OefeningenService os;
+	
+	@GetMapping("/allOefeningen")
+	public Iterable<Oefening> getAllOefening() {
+		Iterable<Oefening> oefeningen = os.vindOefening();
+		return oefeningen; 
+		
+	}
+	
 
 }
