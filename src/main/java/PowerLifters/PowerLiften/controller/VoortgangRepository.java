58967ef1f2cd1b21196a04e1d7b159f203dccef3
MarijnMetzlar <marijn_jacob@hotmail.com>
2020-05-13 15:09:03 +0200

    Voortgang doorsturen WIP

---
+++ src/main/java/PowerLifters/PowerLiften/controller/VoortgangRepository.java
@@ -0,0 +1,9 @@
+package PowerLifters.PowerLiften.controller;
+
+import org.springframework.data.repository.CrudRepository;
+
+import PowerLifters.PowerLiften.domein.Voortgang;
+
+public interface VoortgangRepository extends CrudRepository<Voortgang,Long> {
+
+}
