51fdddffbea78d1aaf14f18945ec17a319dcc697
MarijnMetzlar <marijn_jacob@hotmail.com>
2020-05-14 15:15:16 +0200

    vragenlijst vragen ophalen

--- src/main/java/PowerLifters/PowerLiften/domein/Voortgang.java
+++ src/main/java/PowerLifters/PowerLiften/domein/Voortgang.java
@@ -1,9 +1,12 @@
 package PowerLifters.PowerLiften.domein;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Voortgang {	
@@ -11,11 +14,12 @@ public class Voortgang {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
-	private String oefening;
-	private String oefeningtijd;
+	@ManyToOne
+	private Oefening oefening;
+	private float oefeningtijd;
 	private int liftaantal;
-	private String gebruiktegewicht;
-	private String starttijd;
+	private double gebruiktegewicht;
+	private LocalDateTime starttijd;
 	private String feedback;
 	
 	public String getFeedback() {
@@ -27,16 +31,16 @@ public class Voortgang {
 	public long getId() {
 		return id;
 	}
-	public String getOefening() {
+	public Oefening getOefening() {
 		return oefening;
 	}
-	public void setOefening(String oefening) {
+	public void setOefening(Oefening oefening) {
 		this.oefening = oefening;
 	}
-	public String getOefeningtijd() {
+	public float getOefeningtijd() {
 		return oefeningtijd;
 	}
-	public void setOefeningtijd(String oefeningtijd) {
+	public void setOefeningtijd(float oefeningtijd) {
 		this.oefeningtijd = oefeningtijd;
 	}
 	public int getLiftaantal() {
@@ -45,16 +49,16 @@ public class Voortgang {
 	public void setLiftaantal(int liftaantal) {
 		this.liftaantal = liftaantal;
 	}
-	public String getGebruiktegewicht() {
+	public double getGebruiktegewicht() {
 		return gebruiktegewicht;
 	}
-	public void setGebruiktegewicht(String gebruiktegewicht) {
+	public void setGebruiktegewicht(double gebruiktegewicht) {
 		this.gebruiktegewicht = gebruiktegewicht;
 	}
-	public String getStarttijd() {
+	public LocalDateTime getStarttijd() {
 		return starttijd;
 	}
-	public void setStarttijd(String starttijd) {
+	public void setStarttijd(LocalDateTime starttijd) {
 		this.starttijd = starttijd;
 	}
 }
