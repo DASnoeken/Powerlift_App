83349b85741c514f1e82125fd69c3308338f9ac0
MarijnMetzlar <marijn_jacob@hotmail.com>
2020-05-14 09:31:37 +0200

    Vragenlijst wip

--- src/main/java/PowerLifters/PowerLiften/domein/Voortgang.java
+++ src/main/java/PowerLifters/PowerLiften/domein/Voortgang.java
@@ -50,7 +50,4 @@ public class Voortgang {
 	public void setStarttijd(String starttijd) {
 		this.starttijd = starttijd;
 	}
-	
-	
-	
 }
--- src/main/java/PowerLifters/PowerLiften/domein/Vragenlijst.java
+++ src/main/java/PowerLifters/PowerLiften/domein/Vragenlijst.java
@@ -1,5 +1,30 @@
 package PowerLifters.PowerLiften.domein;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Vragenlijst {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String[] vragen = {"Hoe voel je je na het sporten?", "Vond je het zwaar?"};
+	private String[] antwoorden;
+	
+	public String[] getVragen() {
+		return vragen;
+	}
+	public void setVragen(String[] vragen) {
+		this.vragen = vragen;
+	}
+	public String[] getAntwoorden() {
+		return antwoorden;
+	}
+	public void setAntwoorden(String[] antwoorden) {
+		this.antwoorden = antwoorden;
+	}
 }
