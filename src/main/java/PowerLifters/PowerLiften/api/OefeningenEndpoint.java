461bae996091205644b6f56d6f668c6a34cd8bcd
unknown <d.a.snoeken@protonmail.com>
2020-05-16 22:06:04 +0100

    Update uitleg

--- src/main/java/PowerLifters/PowerLiften/api/OefeningenEndpoint.java
+++ src/main/java/PowerLifters/PowerLiften/api/OefeningenEndpoint.java
@@ -36,7 +36,7 @@ public class OefeningenEndpoint {
 		for(int i = 0; i<namen.length;i++){
 			Oefening oefening = new Oefening();
 			oefening.setNaam(namen[i]);
-			oefening.setUitleg("uitleg");
+			oefening.setUitleg();
 			os.opslaanOefening(oefening);
 		}
 	}
