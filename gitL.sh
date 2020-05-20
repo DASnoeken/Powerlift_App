225b467b875aed4c3a98ba6519865baf23ac20d4
unknown <d.a.snoeken@protonmail.com>
2020-05-20 14:05:39 +0200

    Handig bash script voor git

--- gitL.sh
+++ gitL.sh
@@ -3,6 +3,10 @@ if [ $# -ne 1 ]
 then
 	echo "Voer je naam in als input argument"
 	exit 1
+elif [ $1 != "Daan" ] && [ $1 != "Joeri" ] && [ $1 != "Marijn" ]
+then
+	echo "Ongeldige naam pipo"
+	exit 1
 fi
 
 git checkout master
