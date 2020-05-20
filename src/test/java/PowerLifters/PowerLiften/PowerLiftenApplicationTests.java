c13dcac7a84834be5ca6e5fe4ca267a9665ca196
JoeriJanssen <JoeriJanssen@live.nl>
2020-05-20 16:10:30 +0200

    allemaal research dingen

--- src/test/java/PowerLifters/PowerLiften/PowerLiftenApplicationTests.java
+++ src/test/java/PowerLifters/PowerLiften/PowerLiftenApplicationTests.java
@@ -1,13 +1,22 @@
 package PowerLifters.PowerLiften;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import PowerLifters.PowerLiften.domein.Voortgang;
 
 @SpringBootTest
 class PowerLiftenApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int a = 2;
+		Voortgang v = new Voortgang();
+		v.setLiftaantal(-35);
+		Assertions.assertEquals(v.getLiftaantal(),-35);
+		v.setAntwoorden("hoi");
+		Assertions.assertEquals(v.getAntwoorden(),"hoi");
 	}
 
 }
