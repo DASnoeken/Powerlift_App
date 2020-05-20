2511768b16db9e02067f826fe53f137a7ba64c6d
unknown <d.a.snoeken@protonmail.com>
2020-05-20 22:40:26 +0200

    De coach kan nu uploaden

--- Frontend/Powerliften-coach/src/app/app.module.ts
+++ Frontend/Powerliften-coach/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VoortgangComponent } from './Voortgang/voortgang.component';
+import { OefeningComponent } from './Oefening/oefening.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VoortgangComponent
+    VoortgangComponent,
+    OefeningComponent
   ],
   imports: [
     BrowserModule,
